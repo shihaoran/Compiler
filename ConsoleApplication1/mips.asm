768fefb8ca3b2febd4017c1a01ae6a39e2b15b86
浩然 石 <shhr_2006@126.com>
2016-11-30 08:36:36 +0800

    f

---
+++ ConsoleApplication1/mips.asm
@@ -0,0 +1,12 @@
+.data
+small     :  .word	49
+big       :  .word	17
+z         :  .word	97
+i         :  .space	4
+j         :  .space	4
+op        :  .space	4
+__str0    :  .asciiz	"please input op"
+__str1    :  .asciiz	"please input questiontimes"
+__str2    :  .asciiz	"please input num"
+.text
+j	main
